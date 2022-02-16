607139ad1f7f303623f7983a217f4178fc7ee8ec
Hassan <hassanaziz0012@gmail.com>
2022-02-16 16:34:17 +0500

    Commit after removing git filecache

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kaj_management.wsgi
---
+++ main/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+class Listing(models.Model):
+    address = models.CharField(max_length=300, null=False, blank=False)
+    zip_code = models.IntegerField(null=False, blank=False)
+    city = models.CharField(max_length=100, null=False, blank=False)
+    beds_count = models.IntegerField(default=1)
+    baths_count = models.IntegerField(default=1)
+    images = models.ManyToManyField('ListingImage', related_name='images', blank=True)
+
+    price = models.IntegerField(null=False, blank=False)
+    application_fee = models.IntegerField(null=False, blank=False, default=1)
+    description = models.TextField(null=True, blank=True)
+
+    def get_banner(self):
+        if len(self.images.all()):
+            return self.images.first().file.url
+        else:
+            return None
+
+    def __str__(self) -> str:
+        return f'{self.address} - ${self.price}'
+
+    def __repr__(self) -> str:
+        return f'<Listing: {self.address} - ${self.price}>'
+
+
+class ListingImage(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing')
+    file = models.ImageField(upload_to='listing-images', null=False, blank=False)
+    
+    def __str__(self) -> str:
+        return f'{self.file.name} - {self.listing.address}'
+
+    def __repr__(self) -> str:
+        return f'<ListingImage: {self.file.name} - {self.listing.address}>'
